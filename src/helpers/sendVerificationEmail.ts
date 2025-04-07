33dde34dc9e5e3f990cc6035fd2ee90fffd83f41
isalim079 <salimreza1025@gmail.com>
2025-04-07 09:46:09 +0600

    sendVerification email setup done

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -2,17 +2,22 @@ import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
-export async function secdVerificationEmail (
-    email: string,
-    username: string,
-    verifyCode: string,
-): Promise<ApiResponse>{
-    try {
+export async function secdVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Anonymous message verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
 
-        return {success: true, message: "Failed to send verification email"}
-        
-    } catch (emailError) {
-        console.error("Error sending verification email", emailError);
-        return {success: false, message: 'Failed to send verification email'}
-    }
+    return { success: true, message: "Verification email send successfully" };
+  } catch (emailError) {
+    console.error("Error sending verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
 }
